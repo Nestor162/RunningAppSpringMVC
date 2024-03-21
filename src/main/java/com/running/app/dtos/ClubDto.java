133042483cc7114313d3b89db39c577cf264f0b9
Nestor162 <113930607+Nestor162@users.noreply.github.com>
2024-03-21 10:18:50 +0100

    Add constructor to Club DTO

--- src/main/java/com/running/app/dtos/ClubDto.java
+++ src/main/java/com/running/app/dtos/ClubDto.java
@@ -3,6 +3,7 @@ package com.running.app.dtos;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
@@ -11,7 +12,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
 @Data
-@Builder
+@AllArgsConstructor
 public class ClubDto {
     private Long id;
     private String title;
